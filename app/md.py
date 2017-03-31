940841ea8aeda2dc52792ca4e25fa5dfdb403182
chenbotao828 <85280861@qq.com>
2017-03-31 10:36:45 +0800

    Signed-off-by: chenbotao828 <85280861@qq.com>

--- app/md.py
+++ app/md.py
@@ -5,7 +5,7 @@ from flask.ext.mongoengine.wtf import model_form
 
 class Todo(db.Document):
     content = db.StringField(required=True, max_length=20)
-    time = db.DateTimeField(default=datetime.datetime.now())
+    time = db.DateTimeField(default=datetime.datetime.now)
     status = db.IntField(default=0)
 
 
